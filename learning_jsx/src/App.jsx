4accfb570e143f00c0436e125b3c0450ac566c42
Emediong Bassey Edem <edememediong1@gmail.com>
2024-09-07 10:50:38 +0100

    First Commit

---
+++ learning_jsx/src/App.jsx
@@ -0,0 +1,34 @@
+function ListItem(props){
+  return(
+    <li>{props.animal}</li>
+  )
+
+}
+
+
+
+function List(props){
+  return(
+    <ul>
+        {props.animals.map((animal) =>{
+          return  animal.startsWith("L") ? <ListItem key={animal} animal={animal}/> : null
+        })}
+    </ul>
+  )
+}
+
+
+function App() {
+  
+  const animals = ['Goat', 'Elephant', 'Sheep', 'Guinea pig', 'Lobster', "Lion"];
+
+  return (
+    <div>
+      <h1>Animals:</h1>
+      <List animals={animals}/>
+      
+    </div>
+  )
+}
+
+export default App
